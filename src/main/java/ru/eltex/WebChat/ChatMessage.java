74927a943cd85e66a9622d4b8a5a6f4e73a02e1b
Aleksey <gromov-aleksey@outlook.com>
2019-04-09 15:29:50 +0700

    Добавил front-end

--- src/main/java/ru/eltex/WebChat/ChatMessage.java
+++ src/main/java/ru/eltex/WebChat/ChatMessage.java
@@ -1,7 +1,7 @@
 package ru.eltex.WebChat;
 
 /**
- * s описывает модель сообщения
+ * Класс {@code ChatMessage} описывает модель сообщения
  * @author Aleksey Gromov
  * */
 
@@ -9,26 +9,33 @@ public class ChatMessage {
     private MessageType type;
     private String content;
     private String sender;
+
     public enum MessageType {
         CHAT,
         JOIN,
         LEAVE
     }
+
     public MessageType getType() {
         return type;
     }
+
     public void setType(MessageType type) {
         this.type = type;
     }
+
     public String getContent() {
         return content;
     }
+
     public void setContent(String content) {
         this.content = content;
     }
+
     public String getSender() {
         return sender;
     }
+
     public void setSender(String sender) {
         this.sender = sender;
     }
--- src/main/java/ru/eltex/WebChat/WebChatApplication.java
+++ src/main/java/ru/eltex/WebChat/WebChatApplication.java
@@ -5,9 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebChatApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebChatApplication.class, args);
 	}
 
 }
+
+/**TODO LIST:
+ * 1) css
+ * 2) для удобства разбить index.html на login.html и chat.html
+ * 3) аннотации на main.js и chatController, прописать @version
+ * 4) mysql/mongodb
+ * 5) websecurity
+ * * */
