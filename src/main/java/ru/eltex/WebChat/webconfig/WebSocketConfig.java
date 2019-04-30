2ec51d2b8f2e1327a0a33e7e98dfc1787c6a9e28
Aleksey <gromov-aleksey@outlook.com>
2019-04-30 21:57:16 +0700

    Исправил проблему с пакетом, сделал небольшой рефракторинг страницы чата

--- src/main/java/ru/eltex/WebChat/webconfig/WebSocketConfig.java
+++ src/main/java/ru/eltex/WebChat/webconfig/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package ru.eltex.WebChat.config;
+package ru.eltex.WebChat.webconfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
