49a8ab461c60b43b65e93b39c92414d865a52235
GorodentsevD <gorodentsevd@gmail.com>
2019-05-09 19:14:22 +0700

    Обновил javadoc

--- src/main/java/ru/eltex/WebChat/WebChatApplication.java
+++ src/main/java/ru/eltex/WebChat/WebChatApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.eltex.WebChat.model.ChatMessageModel;
 
+/**
+ * Cтартовый класс приложения
+ */
+
 @SpringBootApplication
 public class WebChatApplication {
 	public static void main(String[] args) { SpringApplication.run(WebChatApplication.class, args); }
